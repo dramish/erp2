0407df21fed3a23e432d9358fc3113549ef167cf
pranatimittal <48480667+pranatimittal@users.noreply.github.com>
2021-03-01 10:43:26 +0530

    Debug

--- faculty_pages/genTestKey.php
+++ faculty_pages/genTestKey.php
@@ -1,5 +1,3 @@
-$query = ;
-
 <?php
 include('config.php');
 session_start();
@@ -9,14 +7,19 @@ if($_SESSION['xy']=='')
 }
 //echo "s2";
 //del test
-if(@$_GET['tname'] ) 
+if(@$_GET['tname'] )
 {
 	//echo "s3";
 
 $var=$_GET['tid'];
-$result = mysqli_query($con,"SELECT testkey FROM quiz where teacher_id='$var' and quizid='$var'") or die('No Entries');
-//echo "s33";
-$r1 = mysqli_query($con,"UPDATE quiz SET duplicateTestKey = '$result' where quizid='$var'") or die('Error1');
+$result = mysqli_query($con,"SELECT testkey FROM quiz where quizid='$var'") or die('No Entries');
+while($row=mysqli_fetch_array($result)) {
+    $tkey = $row['testkey'];
+    //echo "s33";
+    $r1 = mysqli_query($con,"UPDATE quiz SET duplicateTestKey = '$tkey' where quizid='$var'") or die('Error1');
+}
+
+
 //echo "s4";
 //echo "s5";
 header("location:viewtest.php");
