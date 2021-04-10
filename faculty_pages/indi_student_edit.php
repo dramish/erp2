0bb807b6e239315aabe5290b416d0445e402053c
Ishita Bansal <42008217+dramish@users.noreply.github.com>
2021-04-11 01:21:18 +0530

    update attendance

---
+++ faculty_pages/indi_student_edit.php
@@ -0,0 +1,382 @@
+<?php
+include('config.php');
+session_start();
+?>
+<?php
+
+if($_SESSION['xy']=='')
+{
+   echo "<script>window.location.href='faculty_login.php'</script>";
+}
+
+?>
+
+
+<html lang="en">
+<head>
+<link rel = "icon" type = "image/png" href = "IGDTUW-Logo.png">
+<title>View Responses</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+/* Style the body */
+.content {
+  flex: 1 0 auto;
+}  
+body {
+  font-family: Arial;
+  /* margin: 0; */
+}
+
+
+
+ table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+}
+
+
+td, th {
+  border: 2px solid #dddddd;
+
+  padding: 8px;
+}
+tr:nth-child(even){background-color: #f2f2f2;}
+tr:hover {background-color: #ddd;}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}
+/* Header/Logo Title */
+.header {
+  position: relative;
+  padding: 20px;
+  background: white;
+  color: #21610B;  
+  font-size: 15px;
+  
+}
+p{
+color:black;
+font-size:25px;
+}
+
+ .header img {
+  float: left;
+  width: 150px;
+  height: 120px;
+  background: #555;
+  margin-right:15px;
+}
+/*.footer p{
+  position: relative;
+  height: 50px;
+  width: 100%;
+  background-color: black;
+  text-align: center;
+  color:white;
+   font-size:30px;
+  }*/
+ 
+ .registerbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 80%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+ .main h1{
+    font-family: "Times New Roman", Georgia, Serif;
+    font-size: 30px;
+    color:#5e0c17;
+    text-align: center;
+  }
+
+    .navbar {
+  overflow: hidden;
+  background-color:#555;
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: #4CAF50;
+}
+
+
+.manageuser{
+      font-family: "Times New Roman", Times, serif;
+      font-size: 20px;
+      
+    }
+    table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+}
+
+.container {
+  padding: 16px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+input[type=number], select{
+  width: 80%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=date], select{
+  width: 80%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text], select{
+  width: 80%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+td, th {
+  border: 2px solid #dddddd;
+
+  padding: 8px;
+}
+tr:nth-child(even){background-color: #f2f2f2;}
+tr:hover {background-color: #ddd;}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}
+.navbar .icon {
+  display: none;
+}
+@media (max-width: 576px) {
+  
+  .header{
+    font-size:8px;
+  }
+  .header p{
+    font-size:12px;
+  }
+  .header img{
+    width:100px;
+    height:80px;
+ }
+ .main h1{
+     font-size: 20px;
+   }
+   th,td{
+     font-size:12px;
+     padding:5px;
+   }
+   table{
+     width:100%;
+   }
+     .navbar a:not(:first-child) {display: none;}
+  .navbar a.icon {
+    float: right;
+    display: block;
+  }
+  .navbar.responsive {position: relative;}
+  .navbar.responsive .icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .navbar.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .navbar a:hover{
+  background-color: #ddd;
+  color: black;
+}
+
+  .navbar a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+ }
+</style>
+</head>
+<body>
+<a name="top"></a>
+    <div class="content">
+<div class="header">
+  <img src="IGDTUW-logo.png" alt="logo" />
+  <h1>INDIRA GANDHI DELHI TECHNICAL UNIVERSITY FOR WOMEN</h1>
+  <p>(Established by Govt. of Delhi vide Act 9 of 2012)</p>
+</div>    
+
+<div class="navbar" id="myTopnav">
+    <a href="fac_module.php"><i class="fa fa-file-text" aria-hidden="true"></i> Module</a>
+    <a href="fac_classroom_page.php"><i class="fa fa-user  fa-home"></i> Home</a>
+    <a href="<?php echo $meet_url ?>" target="_blank"><i class="fa fa-upload"></i> Initiate class </a>
+<!--     <a href="fac_view_attendance.php"><i class="fa fa-eye" aria-hidden="true"></i> View Attendance</a> -->
+        <a href="fac_batch.php"><i class="fa fa-eye" aria-hidden="true"></i> View Attendance</a>
+
+    <a href="fac_resources.php"><i class="fa fa-upload" aria-hidden="true"></i> Resources Center </a>
+    <a href="fac_display_tt.php"><i class="fa fa-upload" aria-hidden="true"></i>View Timetable</a>
+    <a href="fac_edit_tt.php"><i class="fa fa-upload" aria-hidden="true"></i>Edit timetable</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+  </div>
+
+
+      <script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+</script>
+
+
+<div class="main"><h1>
+
+<?php 
+
+$id=$_SESSION["idf"];
+$qid=$_GET["qid"];
+
+
+
+$r= mysqli_query($con, "SELECT rollno FROM login_student where email='$qid'");
+
+while($ro = mysqli_fetch_array($r)){
+    $a3 = $ro['rollno'];
+
+ echo 'Edit attendance for '.$a3;
+
+}
+?>
+ </h1>
+</div>
+<div class = "manageuser">
+   <form align="center" method="post" enctype="multipart/form-data">
+    <div class="container">
+    <input type="text" name="date" placeholder="Enter date (dd-mm-YYYY)">
+    <input type="number" name="changeNumber" placeholder="Enter attendance">
+    <button name="sub" type="submit" class="registerbtn">Submit Edited Attendance</button>
+    </div>
+  </form>
+
+<?php
+        if(isset($_POST["sub"]))
+        {
+          $date=$_POST["date"];
+          $changeNumber = $_POST["changeNumber"];
+
+  $que= mysqli_query($con, "SELECT * FROM attendance_trial where student_email='$qid' and tid='$id' and date = '$date'");
+
+      if($data=mysqli_fetch_array($que))
+        {
+
+if($changeNumber == 0){
+  $query="DELETE FROM attendance_trial WHERE student_email='$qid' and tid='$id' and date = '$date'";
+      $row=mysqli_query($con, $query);
+}
+
+else{
+$query="UPDATE attendance_trial SET attend='$changeNumber' WHERE student_email='$qid' and tid='$id' and date = '$date'";
+      $row=mysqli_query($con, $query);    }
+
+
+        }
+
+
+        else
+        {
+          //$revDate = REVERSE($date);
+        
+        if($changeNumber == 0){
+          echo '<script>alert("No records for this date");</script>';
+        }
+
+        else{
+             $ins_res=mysqli_query($con,"INSERT INTO attendance_trial (student_email, tid, date, attend) VALUES ('$qid','$id','$date','$changeNumber')") or die(Error);}
+
+        }
+
+// $r2 = mysqli_query($con,"UPDATE subjective_history SET score='$finalScore' WHERE  hid = '$hid' ") or die('ErrorIsh');
+        echo "<script>window.location.href='fac_batch.php'</script>";
+
+  }
+
+
+echo '<table align="center"><th>Date</th><th>Time of joining</th><th>Attendance</th>';
+
+$r= mysqli_query($con, "SELECT * FROM attendance_trial where student_email='$qid' and tid='$id' ORDER BY date");
+
+while($ro = mysqli_fetch_array($r)){
+    $a1 = $ro['date'];
+    $a2 = $ro['time'];
+    $a3 = $ro['attend'];
+    //$a2 = $ro['time'];
+
+ echo '<tr><td>'.$a1.'</td><td>'.$a2.'</td><td>'.$a3.'</td></tr>';
+
+}
+
+echo '</table>';
+
+
+  ?>  
+
+
+
+</div> 
+    
+    <?php
+include('../footer.php');
+?>
+
+</body>
+</html>
